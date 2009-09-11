c82a92fe895b9e5bbc0a88df50418e5a42386857
blaisegassend <devnull@localhost>
2009-09-11 08:02:24 +0000

    sound_play: Renamed SoundHandle to SoundClient. Added Sound-centric C++ API. Changed byte to int8 in msg file. Updated documentation.

--- sound_play/include/sound_play/sound_play.h
+++ sound_play/include/sound_play/sound_play.h
@@ -1,27 +1,149 @@
-#ifndef __SOUND_PLAY__H__
-#define __SOUND_PLAY__H__
+#ifndef __SOUND_PLAY__SOUND_PLAY__H__
+#define __SOUND_PLAY__SOUND_PLAY__H__
 
 #include <string>
 #include <ros/ros.h>
 #include <ros/node_handle.h>
 #include <sound_play/SoundRequest.h>
+#include <boost/thread.hpp>
 
 namespace sound_play
 {
 
-/** 
+/// Using SoundHandle is deprecated. The new name is SoundClient.
+#define SoundHandle SoundClient
+
+/**
+ * \brief Class that publishes messages to the sound_play node.
+ *
  * This class is a helper class for communicating with the sound_play node
- * via the \ref sound_play::SoundRequest message. There is a one-to-one mapping
- * between methods and invocations of the \ref sound_play::SoundRequest message.
+ * via the \ref sound_play::SoundRequest message. It has two ways of being used:
+ *
+ * - It can create Sound classes that represent a particular sound which
+ *   can be played, repeated or stopped.
+ *
+ * - It provides methods for each way in which the sound_play::SoundRequest
+ *   message can be invoked.
  */
 
-class SoundHandle
+class SoundClient
 {
 public:
-  SoundHandle()
+	class Sound
+	{
+		friend class SoundClient;
+	private:
+		int snd_;
+		std::string arg_;
+		SoundClient *client_;
+
+		Sound(SoundClient *sc, int snd, const std::string &arg)
+		{
+			client_ = sc;
+			snd_ = snd;
+			arg_ = arg;
+		}
+
+	public:
+		/**
+		 * \brief Play the Sound.
+		 *
+		 * This method causes the Sound to be played once.
+		 */
+
+		void play()
+		{
+			client_->sendmsg(snd_, SoundRequest::PLAY_ONCE, arg_);
+		}
+
+		/**
+		 * \brief Play the Sound repeatedly.
+		 *
+		 * This method causes the Sound to be played repeatedly until stop() is
+		 * called.
+		 */
+
+		void repeat()
+		{
+			client_->sendmsg(snd_, SoundRequest::PLAY_START, arg_);
+		}
+
+		/**
+		 * \brief Stop Sound playback.
+		 *
+		 * This method causes the Sound to stop playing.
+		 */
+
+		void stop()
+		{
+			client_->sendmsg(snd_, SoundRequest::PLAY_STOP, arg_);
+		}
+	};
+	
+/** \brief Create a SoundClient that publishes on the given topic
+ *
+ * Creates a SoundClient that publishes to the given topic relative to the
+ * given NodeHandle.
+ *
+ * \param nh Node handle to use when creating the topic.
+ *
+ * \param topic Topic to publish to.
+ */
+
+	SoundClient(ros::NodeHandle &nh, const std::string &topic)
   {
-    pub_ = ros::NodeHandle().advertise<sound_play::SoundRequest>("/robotsound", 1);
+		init(nh, topic);
+	}
+
+/** \brief Create a SoundClient with the default topic
+ *
+ * Creates a SoundClient that publishes to "robotsound".
+ */
+
+	SoundClient()
+  {
+		init(ros::NodeHandle(), "robotsound");
   }
+
+/**
+ * \brief Create a voice Sound.
+ *
+ * Creates a Sound corresponding to saying the indicated text.
+ *
+ * \param s Text to say
+ */
+
+  Sound voiceSound(const std::string &s)
+	{
+		return Sound(this, SoundRequest::SAY, s);
+	}
+
+/**
+ * \brief Create a wave Sound.
+ *
+ * Creates a Sound corresponding to indicated file.
+ *
+ * \param s File to play. Should be an absolute path that exists on the
+ * machine running the sound_play node.
+ */
+
+  Sound waveSound(const std::string &s)
+	{
+		return Sound(this, SoundRequest::PLAY_FILE, s);
+	}
+
+/**
+ * \brief Create a builtin Sound.
+ *
+ * Creates a Sound corresponding to indicated builtin wave.
+ *
+ * \param id Identifier of the sound to play.
+ */
+
+  Sound builtinSound(int id)
+	{
+		return Sound(this, id, "");
+	}
 
 /** \brief Say a string
  *
@@ -31,7 +153,7 @@ public:
  * \param s String to say
  */
 	
-	void say(const std::string s)
+	void say(const std::string &s)
   {
     sendmsg(SoundRequest::SAY, SoundRequest::PLAY_ONCE, s);
   }
@@ -43,7 +165,7 @@ public:
  * \param s String to say repeatedly
  */
 	
-  void repeat(const std::string s)
+  void repeat(const std::string &s)
   {
     sendmsg(SoundRequest::SAY, SoundRequest::PLAY_START, s);
   }
@@ -56,7 +178,7 @@ public:
  * \param s Same string as in the say or repeat command
  */
 
-  void stopsaying(const std::string s)
+  void stopsaying(const std::string &s)
   {
     sendmsg(SoundRequest::SAY, SoundRequest::PLAY_STOP, s);
   }
@@ -70,7 +192,7 @@ public:
  * on the computer on which the sound_play node is running
  */
 
-  void playwave(const std::string s)
+  void playwave(const std::string &s)
   {
     sendmsg(SoundRequest::PLAY_FILE, SoundRequest::PLAY_ONCE, s);
   }
@@ -83,7 +205,7 @@ public:
  * on the computer on which the sound_play node is running.
  */
 
-  void startwave(const std::string s)
+  void startwave(const std::string &s)
   {
     sendmsg(SoundRequest::PLAY_FILE, SoundRequest::PLAY_START, s);
   }
@@ -96,7 +218,7 @@ public:
  * \param s Same string as in the playwave or startwave command
  */
 
-  void stopwave(const std::string s)
+  void stopwave(const std::string &s)
   {
     sendmsg(SoundRequest::PLAY_FILE, SoundRequest::PLAY_STOP, s);
   }
@@ -149,18 +271,47 @@ public:
 		stop(SoundRequest::ALL);
 	}
 
+  /** \brief Turns warning messages on or off.
+	 *  
+	 * If a message is sent when no node is subscribed to the topic, a
+	 * warning message is printed. This method can be used to enable or
+	 * disable warnings.
+	 *
+	 * \param state True to turn off messages, false to turn them on.
+	 */
+
+  void setQuiet(bool state)
+	{
+		quiet_ = state;
+	}
+
 private:
-  void sendmsg(int snd, int cmd, std::string s = "")
+	void init(ros::NodeHandle nh, const std::string &topic)
+	{
+    pub_ = nh.advertise<sound_play::SoundRequest>(topic, 5);
+		quiet_ = false;
+  }
+
+	void sendmsg(int snd, int cmd, const std::string &s = "")
   {
-    SoundRequest msg;
+		boost::mutex::scoped_lock lock(mutex_);
+    
+		SoundRequest msg;
     msg.sound = snd;
     msg.command = cmd;
     msg.arg = s;
-    pub_.publish(msg);
+		pub_.publish(msg);
+
+		if (pub_.getNumSubscribers() == 0 && !quiet_)
+			ROS_WARN("Sound command issued, but no node is subscribed to the topic.");
   }
-  
+
+	bool quiet_;
   ros::Publisher pub_;
+	boost::mutex mutex_;
 };
+
+typedef SoundClient::Sound Sound;
 };
 
 #endif
--- sound_play/test/test.cpp
+++ sound_play/test/test.cpp
@@ -6,33 +6,51 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "sound_play_test");
 
   ros::NodeHandle nh;
-  sound_play::SoundHandle h;
+  sound_play::SoundClient sc;
 
   sleep(1);
   
   while(nh.ok())
   {
-    h.say("Hello world!");
+    sc.say("Hello world!");
     sleep(2);
 
     const char *str1 = "I am annoying.";
-    h.repeat(str1);
+    sc.repeat(str1);
     sleep(4);
-    h.stopsaying(str1);
+    sc.stopsaying(str1);
 
-    h.playwave("/usr/share/xemacs21/xemacs-packages/etc/sounds/boing.wav");
+    sc.playwave("/usr/share/xemacs21/xemacs-packages/etc/sounds/boing.wav");
     sleep(2);
 
     const char *str2 = "/usr/share/xemacs21/xemacs-packages/etc/sounds/piano-beep.wav";
-    h.startwave(str2);
+    sc.startwave(str2);
     sleep(4);
-    h.stopwave(str2);
+    sc.stopwave(str2);
 
-    h.play(sound_play::SoundRequest::NEEDS_UNPLUGGING);
+    sc.play(sound_play::SoundRequest::NEEDS_UNPLUGGING);
     sleep(2);
 
-    h.start(sound_play::SoundRequest::BACKINGUP);
+    sc.start(sound_play::SoundRequest::BACKINGUP);
     sleep(4);
-    h.stop(sound_play::SoundRequest::BACKINGUP);
+    sc.stop(sound_play::SoundRequest::BACKINGUP);
+
+		sleep(2);
+		sound_play::Sound s1 = sc.waveSound("/usr/share/xemacs21/xemacs-packages/etc/sounds/boing.wav");
+		s1.repeat();
+		sleep(1);
+		s1.stop();
+		
+		sleep(2);
+		sound_play::Sound s2 = sc.voiceSound("This is a really long sentence that will get cut off.");
+		s2.play();
+		sleep(1);
+		s2.stop();
+
+		sleep(2);
+		sound_play::Sound s3 = sc.builtinSound(sound_play::SoundRequest::NEEDS_UNPLUGGING_BADLY);
+		s3.play();
+		sleep(1);
+		s3.stop();
   }
 }
