55ab08c882809fc6d21affb849a7dac9f1901867
Felix Duvallet <felix.duvallet@epfl.ch>
2015-11-04 11:26:58 +0100

    Set the volume in each of the sound_play actionlib tests. This makes the script actually play the sounds it requests.

--- sound_play/scripts/test_actionlib_client.py
+++ sound_play/scripts/test_actionlib_client.py
@@ -7,7 +7,7 @@ from sound_play.msg import SoundRequest, SoundRequestAction, SoundRequestGoal
 
 import os
 
-def sound_play_client():
+def sound_play_client(volume=1.0):
     client = actionlib.SimpleActionClient('sound_play', SoundRequestAction)
 
     client.wait_for_server()
@@ -16,6 +16,7 @@ def sound_play_client():
     goal = SoundRequestGoal()
     goal.sound_request.sound = SoundRequest.NEEDS_UNPLUGGING
     goal.sound_request.command = SoundRequest.PLAY_ONCE
+    goal.sound_request.volume = volume
 
     client.send_goal(goal)
     client.wait_for_result()
@@ -27,6 +28,7 @@ def sound_play_client():
     goal = SoundRequestGoal()
     goal.sound_request.sound = SoundRequest.NEEDS_PLUGGING
     goal.sound_request.command = SoundRequest.PLAY_ONCE
+    goal.sound_request.volume = volume
     client.send_goal(goal)
     client.wait_for_result()
     print client.get_result()
@@ -38,6 +40,7 @@ def sound_play_client():
     goal.sound_request.sound = SoundRequest.SAY
     goal.sound_request.command = SoundRequest.PLAY_ONCE
     goal.sound_request.arg = "Testing the actionlib interface A P I"
+    goal.sound_request.volume = volume
     client.send_goal(goal)
     client.wait_for_result()
     print client.get_result()
@@ -49,6 +52,7 @@ def sound_play_client():
     goal.sound_request.sound = SoundRequest.PLAY_FILE
     goal.sound_request.command = SoundRequest.PLAY_ONCE
     goal.sound_request.arg = os.path.join(roslib.packages.get_pkg_dir('sound_play'),'sounds') + "/say-beep.wav"
+    goal.sound_request.volume = volume
     client.send_goal(goal)
     client.wait_for_result()
     print client.get_result()
