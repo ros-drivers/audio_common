90201f3c8d6d90b51a0a739ee30d947e7a0c1988
aaugsten <42334738+aaugsten@users.noreply.github.com>
2018-08-13 08:22:40 +0200

    Update setup.py

--- sound_play/setup.py
+++ sound_play/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
       packages=['sound_play'],
-      package_dir={'': 'src'}
+      package_dir={'': 'src/sound_play'}
       )
 
 setup(**d)
