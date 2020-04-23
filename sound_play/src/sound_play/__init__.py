d26805e48210fa43ff1a2371e4f82d111147e423
Shingo Kitagawa <shingogo@hotmail.co.jp>
2020-04-24 06:57:06 +0900

    use setuptools instead of distutils.core

--- sound_play/src/sound_play/__init__.py
+++ sound_play/src/sound_play/__init__.py
@@ -30,4 +30,4 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-import libsoundplay as libsoundplay
+import sound_play.libsoundplay as libsoundplay
