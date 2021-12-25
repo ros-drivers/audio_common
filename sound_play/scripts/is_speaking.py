cced53afc7123731e00f6acacb87c37858bc052a
iory <ab.ioryz@gmail.com>
2021-12-25 21:13:27 +0900

    Fixed name speeching to speaking

--- sound_play/scripts/is_speaking.py
+++ sound_play/scripts/is_speaking.py
@@ -6,10 +6,10 @@ import rospy
 import std_msgs.msg
 
 
-class IsSpeeching(object):
+class IsSpeaking(object):
 
     def __init__(self):
-        super(IsSpeeching, self).__init__()
+        super(IsSpeaking, self).__init__()
 
         self.sub = rospy.Subscriber(
             '~robotsound',
@@ -17,25 +17,25 @@ class IsSpeeching(object):
             callback=self.callback,
             queue_size=1)
 
-        self.is_speeching = False
+        self.is_speaking = False
         self.pub_speech_flag = rospy.Publisher(
-            '~output/is_speeching',
+            '~output/is_speaking',
             std_msgs.msg.Bool, queue_size=1)
 
         rospy.Timer(rospy.Duration(0.01), self.speech_timer_cb)
 
     def callback(self, msg):
         if any([len(status.text) > 0 for status in msg.status_list]):
-            self.is_speeching = True
+            self.is_speaking = True
         else:
-            self.is_speeching = False
+            self.is_speaking = False
 
     def speech_timer_cb(self, timer):
         self.pub_speech_flag.publish(
-            std_msgs.msg.Bool(self.is_speeching))
+            std_msgs.msg.Bool(self.is_speaking))
 
 
 if __name__ == '__main__':
-    rospy.init_node('is_speeching')
-    IsSpeeching()
+    rospy.init_node('is_speaking')
+    IsSpeaking()
     rospy.spin()
