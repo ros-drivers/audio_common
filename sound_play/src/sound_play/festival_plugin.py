b0207ecf830fa7b944cbed08d0e98580b24db43b
Shingo Kitagawa <shingogo@hotmail.co.jp>
2022-05-14 18:31:05 +0900

    refactor FestivalPlugin

--- sound_play/src/sound_play/festival_plugin.py
+++ sound_play/src/sound_play/festival_plugin.py
@@ -36,9 +36,9 @@ class FestivalPlugin(SoundPlayPlugin):
                 else:
                     txtfile.write(text.encode('UTF-8'))
             txtfile.flush()
-            os.system(
-                "text2wave -eval '({0})' {1} -o {2}".format(
-                    voice, txtfilename, wavfilename))
+            cmd = "text2wave -eval '({0})' {1} -o {2}".format(
+                voice, txtfilename, wavfilename)
+            os.system(cmd)
             try:
                 if os.stat(wavfilename).st_size == 0:
                     # So we hit the same catch block
